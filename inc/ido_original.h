4dd4a319b1b0b6033dfdaaec1501a22b52f0a399
VVBondarenko <vbondarenko3950@yandex.ru>
2017-01-28 01:17:24 +0300

    finally achieved 4th order of accuracy added some kinds of solvers

--- inc/ido_original.h
+++ inc/ido_original.h
@@ -19,6 +19,9 @@ void IDO_IterationModified_w_f (Grid *Task, double omega, Grid *force);
 void IDO_IterationSet          (Grid *Task, double omega, int N);
 void IDO_Mod_IterationSet_w_f  (Grid *Task, double omega, Grid *force, int N);
 void IDO_Ori_IterationSet_w_f  (Grid *Task, double omega, Grid *force, int N);
-void IDO_Ori_IterationSet_w_f_2(Grid *Task, double omega, Grid *force, int N);
+void IDO_Ori_IterationSet_w_f_Jacoby(Grid *Task, double omega, Grid *force, int N);
+void IDO_Ori_IterationSet_w_f_Jacoby_adapt(Grid *Task, double omega, Grid *force, int N, double (*f_exact)(double, double));
+void IDO_Ori_IterationSet_w_f_Seidel(Grid *Task, double omega, Grid *force, int N);
+void IDO_Ori_IterationSet_w_f_Seidel_ILU(Grid *Task, double omega, Grid *force, int N);
 
 #endif //IDO4POISSONEQ_IDO_ORIGINAL_H
--- src/ido_original.c
+++ src/ido_original.c
@@ -647,7 +647,7 @@ void IDO_Ori_IterationSet_w_f  (Grid *Task, double omega, Grid *force, int N)
     }
 }
 
-void IDO_Ori_IterationSet_w_f_2(Grid *Task, double omega, Grid *force, int N)
+void IDO_Ori_IterationSet_w_f_Jacoby(Grid *Task, double omega, Grid *force, int N)
 {
     int K;
 
@@ -671,8 +671,8 @@ void IDO_Ori_IterationSet_w_f_2(Grid *Task, double omega, Grid *force, int N)
     for(K=0;K<N;K++)
     {
         //        IDO_IterationOriginal_w_f(Task,omega,force);
-//#pragma omp simd
-#pragma omp parallel for shared(nU,ndUdx,ndUdxdy,ndUdy,h,omega,Task,force) private(i,j)
+//#pragma omp parallel for shared(nU,ndUdx,ndUdxdy,ndUdy,h,omega,Task,force) private(i,j)
+#pragma omp simd
         for(i=1; i<Task->n-1; i++)
         {
             for(j=1; j<Task->n-1; j++)
@@ -682,19 +682,39 @@ void IDO_Ori_IterationSet_w_f_2(Grid *Task, double omega, Grid *force, int N)
                         +Task->U[i][j+1]+Task->U[i][j-1])
                         -0.5/h*(Task->dUdx[i+1][j]-Task->dUdx[i-1][j]
                         +Task->dUdy[i][j+1]-Task->dUdy[i][j-1])-force->U[i][j])/8.*h*h;
-
+            }
+        }
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
                 ndUdx[i][j] = (1-omega)*Task->dUdx[i][j]
                         +omega*(7.5*(Task->U[i+1][j]-Task->U[i-1][j])/h/h/h
                         -1.5*(Task->dUdx[i+1][j]+Task->dUdx[i-1][j])/h/h
                         +2.*(Task->dUdx[i][j+1]+Task->dUdx[i][j-1])/h/h
                         -0.5*(Task->dUdxdy[i][j+1]-Task->dUdxdy[i][j-1])/h-force->dUdx[i][j])
                         /(12/h/h+4/h/h);
+            }
+        }
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
                 ndUdy[i][j] = (1-omega)*Task->dUdy[i][j]
                         +omega*(7.5*(Task->U[i][j+1]-Task->U[i][j-1])/h/h/h
                         -1.5*(Task->dUdy[i][j+1]+Task->dUdy[i][j-1])/h/h
                         +2.*(Task->dUdy[i+1][j]+Task->dUdy[i-1][j])/h/h
                         -0.5*(Task->dUdxdy[i+1][j]-Task->dUdxdy[i-1][j])/h-force->dUdy[i][j])
                         /(12/h/h+4/h/h);
+            }
+        }
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
                 ndUdxdy[i][j] = (1-omega)*Task->dUdxdy[i][j]
                         +omega*(7.5*(Task->dUdx[i][j+1]-Task->dUdx[i][j-1]
                         +Task->dUdy[i+1][j]-Task->dUdy[i-1][j])/h/h/h
@@ -733,5 +753,290 @@ void IDO_Ori_IterationSet_w_f_2(Grid *Task, double omega, Grid *force, int N)
     free((void *)ndUdy);
     free((void *)ndUdxdy);
 
+
+}
+
+void IDO_Ori_IterationSet_w_f_Seidel(Grid *Task, double omega, Grid *force, int N)
+{
+    int K;
+
+    int i,j;
+    double h = Task->h;
+
+    for(K=0;K<N;K++)
+    {
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+            for(j=1; j<Task->n-1; j++)
+                Task->U[i][j] = (1-omega)*Task->U[i][j]
+                        +omega*(2/h/h*(Task->U[i+1][j]+Task->U[i-1][j]
+                        +Task->U[i][j+1]+Task->U[i][j-1])
+                        -0.5/h*(Task->dUdx[i+1][j]-Task->dUdx[i-1][j]
+                        +Task->dUdy[i][j+1]-Task->dUdy[i][j-1])-force->U[i][j])/8.*h*h;
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+            for(j=1; j<Task->n-1; j++)
+                Task->dUdx[i][j] = (1-omega)*Task->dUdx[i][j]
+                        +omega*(7.5*(Task->U[i+1][j]-Task->U[i-1][j])/h/h/h
+                        -1.5*(Task->dUdx[i+1][j]+Task->dUdx[i-1][j])/h/h
+                        +2.*(Task->dUdx[i][j+1]+Task->dUdx[i][j-1])/h/h
+                        -0.5*(Task->dUdxdy[i][j+1]-Task->dUdxdy[i][j-1])/h-force->dUdx[i][j])
+                        /(12/h/h+4/h/h);
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+            for(j=1; j<Task->n-1; j++)
+                Task->dUdy[i][j] = (1-omega)*Task->dUdy[i][j]
+                        +omega*(7.5*(Task->U[i][j+1]-Task->U[i][j-1])/h/h/h
+                        -1.5*(Task->dUdy[i][j+1]+Task->dUdy[i][j-1])/h/h
+                        +2.*(Task->dUdy[i+1][j]+Task->dUdy[i-1][j])/h/h
+                        -0.5*(Task->dUdxdy[i+1][j]-Task->dUdxdy[i-1][j])/h-force->dUdy[i][j])
+                        /(12/h/h+4/h/h);
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+            for(j=1; j<Task->n-1; j++)
+                Task->dUdxdy[i][j] = (1-omega)*Task->dUdxdy[i][j]
+                        +omega*(7.5*(Task->dUdx[i][j+1]-Task->dUdx[i][j-1]
+                        +Task->dUdy[i+1][j]-Task->dUdy[i-1][j])/h/h/h
+                        -1.5*(Task->dUdxdy[i][j+1]+Task->dUdxdy[i][j-1]
+                        +Task->dUdxdy[i+1][j]+Task->dUdxdy[i-1][j])/h/h
+                        -force->dUdxdy[i][j]
+                        )/24*h*h;
+
+        IDO_InitNormalD_2(Task,force);
+    }
+}
+
+void IDO_Ori_IterationSet_w_f_Seidel_ILU(Grid *Task, double omega, Grid *force, int N)
+{
+    int K;
+
+    int i,j,l;
+    double h = Task->h;
+
+    double **uy, **uxy, **uyy, **uxyy;
+    uy    = malloc(Task->n * sizeof(double*));
+    uxy = malloc(Task->n * sizeof(double*));
+    uyy = malloc(Task->n * sizeof(double*));
+    uxyy = malloc(Task->n * sizeof(double*));
+    for(i=0; i<Task->n; i++)
+    {
+        uy[i]    = malloc(Task->n * sizeof(double));
+        uxy[i] = malloc(Task->n * sizeof(double));
+        uyy[i] = malloc(Task->n * sizeof(double));
+        uxyy[i] = malloc(Task->n * sizeof(double));
+    }
+
+    for(i=0; i<Task->n; i++)
+    {
+        for(j=0; j<Task->n; j++)
+        {
+            uy[i][j] = Task->U[i][j];
+            uxy[i][j] = Task->dUdx[i][j];
+            uyy[i][j] = Task->dUdy[i][j];
+            uxyy[i][j] = Task->dUdxdy[i][j];
+        }
+    }
+    for(K=0;K<N;K++)
+    {
+#pragma omp parallel for shared(uy,uxy,uyy,uxyy,h,force) private(i,j)
+            for(i=1; i<Task->n-1; i++)
+            {
+                for(j=1; j<Task->n-1; j++)
+                {
+                    uy[i][j] = (2/h/h*(uy[i-1][j]+uy[i][j-1])-force->U[i][j])/8.*h*h;
+
+                    uxy[i][j] = (7.5*(-uy[i-1][j])/h/h/h
+                            -1.5*(uxy[i-1][j])/h/h
+                            +2.*(uxy[i][j-1])/h/h
+                            -force->dUdx[i][j])
+                            /(12/h/h+4/h/h);
+                    uyy[i][j] = (7.5*(uy[i][j+1]-uy[i][j-1])/h/h/h
+                            -1.5*(uyy[i][j-1])/h/h
+                            +2.*(uyy[i-1][j])/h/h
+                            -force->dUdy[i][j])
+                            /(12/h/h+4/h/h);
+                    uxyy[i][j] = (7.5*(uxy[i][j+1]-uxy[i][j-1]
+                            +uyy[i+1][j]-uyy[i-1][j])/h/h/h
+                            -1.5*(uxyy[i][j-1]
+                            +uxyy[i-1][j])/h/h
+                            -force->dUdxdy[i][j]
+                            )/24*h*h;
+                }
+            }
+#pragma omp parallel for shared(uy,uxy,uyy,uxyy,h,force) private(i,j)
+            for(i=1; i<Task->n-1; i++)
+            {
+                for(j=1; j<Task->n-1; j++)
+                {
+                    Task->U[i][j] = (2/h/h*(Task->U[i+1][j]+Task->U[i][j+1])
+                            -0.5/h*(Task->dUdx[i+1][j]-Task->dUdx[i-1][j]
+                            +Task->dUdy[i][j+1]-Task->dUdy[i][j-1]))/8.*h*h + uy[i][j];
+
+                    Task->dUdx[i][j] = (
+                                -1.5*(Task->dUdx[i+1][j])/h/h
+                            +2.*(Task->dUdx[i][j+1])/h/h
+                            -0.5*(Task->dUdxdy[i][j+1]-Task->dUdxdy[i][j-1])/h)
+                            /(12/h/h+4/h/h) + uxy[i][j];
+                    Task->dUdy[i][j] = (
+                                -1.5*(Task->dUdy[i][j+1])/h/h
+                            +2.*(Task->dUdy[i+1][j])/h/h
+                            -0.5*(Task->dUdxdy[i+1][j]-Task->dUdxdy[i-1][j])/h)
+                            /(12/h/h+4/h/h)+uyy[i][j];
+                    Task->dUdxdy[i][j] = (
+                                -1.5*(Task->dUdxdy[i][j+1]
+                                +Task->dUdxdy[i+1][j])/h/h
+                            )/24*h*h + uxyy[i][j];
+                }
+            }
+        // update the data in Task
+//        for(i=1; i<Task->n-1; i++)
+//        {
+//            for(j=1; j<Task->n-1; j++)
+//            {
+//                Task->U[i][j] = nU[i][j];
+//                Task->dUdx[i][j] = ndUdx[i][j];
+//                Task->dUdy[i][j] = ndUdy[i][j];
+//                Task->dUdxdy[i][j] = ndUdxdy[i][j];
+//            }
+//        }
+        //        IDO_InitDeriv_2(Task);
+        IDO_InitNormalD_2(Task,force);
+    }
+}
+
+void IDO_Ori_IterationSet_w_f_Jacoby_adapt(Grid *Task, double omega, Grid *force, int N, double (*f_exact)(double, double))
+{
+    int K;
+
+    int i,j;
+    //define new temporary arrays for U, dUdx, dUdy on (n+1) iteration
+    double **nU, **ndUdx, **ndUdy, **ndUdxdy;
+    nU    = malloc(Task->n * sizeof(double*));
+    ndUdx = malloc(Task->n * sizeof(double*));
+    ndUdy = malloc(Task->n * sizeof(double*));
+    ndUdxdy = malloc(Task->n * sizeof(double*));
+    for(i=0; i<Task->n; i++)
+    {
+        nU[i]    = malloc(Task->n * sizeof(double));
+        ndUdx[i] = malloc(Task->n * sizeof(double));
+        ndUdy[i] = malloc(Task->n * sizeof(double));
+        ndUdxdy[i] = malloc(Task->n * sizeof(double));
+    }
+    //for all values in U apply scheme
+    double h = Task->h, du = 0., du_prev = 1.;
+//    double omega = 1.;
+    for(K=0;K<N;K++)
+    {
+        //        IDO_IterationOriginal_w_f(Task,omega,force);
+//#pragma omp parallel for shared(nU,ndUdx,ndUdxdy,ndUdy,h,omega,Task,force) private(i,j)
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
+                nU[i][j] = (1-omega)*Task->U[i][j]
+                        +omega*(2/h/h*(Task->U[i+1][j]+Task->U[i-1][j]
+                        +Task->U[i][j+1]+Task->U[i][j-1])
+                        -0.5/h*(Task->dUdx[i+1][j]-Task->dUdx[i-1][j]
+                        +Task->dUdy[i][j+1]-Task->dUdy[i][j-1])-force->U[i][j])/8.*h*h;
+            }
+        }
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
+                ndUdx[i][j] = (1-omega)*Task->dUdx[i][j]
+                        +omega*(7.5*(Task->U[i+1][j]-Task->U[i-1][j])/h/h/h
+                        -1.5*(Task->dUdx[i+1][j]+Task->dUdx[i-1][j])/h/h
+                        +2.*(Task->dUdx[i][j+1]+Task->dUdx[i][j-1])/h/h
+                        -0.5*(Task->dUdxdy[i][j+1]-Task->dUdxdy[i][j-1])/h-force->dUdx[i][j])
+                        /(12/h/h+4/h/h);
+            }
+        }
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
+                ndUdy[i][j] = (1-omega)*Task->dUdy[i][j]
+                        +omega*(7.5*(Task->U[i][j+1]-Task->U[i][j-1])/h/h/h
+                        -1.5*(Task->dUdy[i][j+1]+Task->dUdy[i][j-1])/h/h
+                        +2.*(Task->dUdy[i+1][j]+Task->dUdy[i-1][j])/h/h
+                        -0.5*(Task->dUdxdy[i+1][j]-Task->dUdxdy[i-1][j])/h-force->dUdy[i][j])
+                        /(12/h/h+4/h/h);
+            }
+        }
+#pragma omp simd
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
+                ndUdxdy[i][j] = (1-omega)*Task->dUdxdy[i][j]
+                        +omega*(7.5*(Task->dUdx[i][j+1]-Task->dUdx[i][j-1]
+                        +Task->dUdy[i+1][j]-Task->dUdy[i-1][j])/h/h/h
+                        -1.5*(Task->dUdxdy[i][j+1]+Task->dUdxdy[i][j-1]
+                        +Task->dUdxdy[i+1][j]+Task->dUdxdy[i-1][j])/h/h
+                        -force->dUdxdy[i][j]
+                        )/24*h*h;
+            }
+        }
+
+/*        for(i=1; i<Task->n-1; i++)
+            for(j=1; j<Task->n-1; j++)
+                du += (Task->U[i][j]-nU[i][j])*(Task->U[i][j]-nU[i][j]);
+
+        du = sqrt(du);
+        */
+        du = 0.;
+        for(i=1; i<Task->n-1; i++)
+            for(j=1; j<Task->n-1; j++)
+                du += fabs(Task->U[i][j]-(*f_exact)(Task->x0+i*h,Task->y0+j*h));
+        if(K%100==0 && K>=1000) du_prev = du;
+
+        //            du_prev = du;
+        // update the data in Task
+//        if(du/1.1 > du_prev && K>=1000)
+//        {
+//            omega -= 0.5;
+//            K = 0;
+//        }
+//        else
+//        {
+        for(i=1; i<Task->n-1; i++)
+        {
+            for(j=1; j<Task->n-1; j++)
+            {
+                Task->U[i][j] = nU[i][j];
+                Task->dUdx[i][j] = ndUdx[i][j];
+                Task->dUdy[i][j] = ndUdy[i][j];
+                Task->dUdxdy[i][j] = ndUdxdy[i][j];
+            }
+        }
+        IDO_InitNormalD_2(Task,force);
+        if(K%100 == 0)
+        {
+            printf("%d ",K);
+            Grid_print_error(Task,f_exact);
+        }
+        //        }
+//        if(omega <= 0.)
+//            break;
+        //        IDO_InitDeriv_2(Task);
+    }
+
+    printf("%d\n%f\t%f\n",K,omega,du);
+    //clean temporary arrays
+    for(i=0; i<Task->n; i++)
+    {
+        free((void *)nU[i]);
+        free((void *)ndUdx[i]);
+        free((void *)ndUdy[i]);
+        free((void *)ndUdxdy[i]);
+    }
+    free((void *)nU);
+    free((void *)ndUdx);
+    free((void *)ndUdy);
+    free((void *)ndUdxdy);
 
 }
